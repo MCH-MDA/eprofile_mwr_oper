f7c0a097df7371197945330938d8127af554c38b
rruefenacht <rruefenacht@users.noreply.github.com>
2023-04-14 16:41:11 +0200

    add script for setting up mars

---
+++ ewc_setup/mars_setup.sh
@@ -0,0 +1,10 @@
+echo "Setting up mars"
+echo "  !!! CARE: Make sure to have your API key registered on this machine !!!"
+echo "      get it in your ECMWF account at https://apps.ecmwf.int/v1/key"
+echo "      then do the following: mars --token MY_API_KEY --email MY_EMAIL"
+
+echo "Setting up webmars"
+echo "  !!! CARE: make sure to remove webmars settings once EWC has moved to Bologna !"
+echo "export MARS_ENVIRON_ORIGIN=webmars" >> ~/.profile
+echo "export MARS_ENVIRON_ORIGIN=webmars" >> ~/.bashrc
+
