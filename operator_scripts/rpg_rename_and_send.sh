bbe296aa1b20a5a87dde56ea5d869f93b857a98c
rruefenacht <rruefenacht@users.noreply.github.com>
2023-05-01 18:09:15 +0200

    documentation

--- operator_scripts/rpg_rename_and_send.sh
+++ operator_scripts/rpg_rename_and_send.sh
@@ -1,16 +1,15 @@
 cycle_start_min=1  # minute when first new cycle of the day starts
 cycle_duration_min=10  # duration of one measurement cycle in minutes
-consider_last_n_min=10  # consider files generated in the last minutesnormally equal to cycle duration  
-data_dir=testdir/
-outdir=testdir/data_for_eprofile
-timestamp_style=min_in  #min_in: smallest input timestamp matching period; cycle_start: assumed start of measurement cycle
+consider_last_n_min=10  # consider files generated in the last minutes normally equal to cycle duration. For longer periods make sure to use timestamp_style=cycle_start 
+data_dir=../data/rpg_rename/
+outdir=../data/rpg_rename/out/
+timestamp_style=cycle_start  # cycle_start: assumed start of measurement cycle; min_in: smallest input timestamp matching period. CARE: min_in only works if consider_last_n_min makes sure that files from only one obs cycle are consdiered 
 
-
+# START OF FUNCTION DEFINITIONS
 function filename2epoch {
     local filename=$1
     local len_ext=${2:-4}
     
-	echo $len_ext
     local ts_sec=00
     local ts_min=${filename:(-$(($len_ext+2))):2}
     local ts_hour=${filename:(-$(($len_ext+4))):2}
@@ -23,11 +22,10 @@ function epoch2timestamp {
 	local ts_epoch=$1
 	stamp=$(date --utc -d "@$ts_epoch" +"%Y%m%d%H%M")
 }
+# END OF FUNCTION DEFINITIONS
 
-#filename2epoch log_202303311510.txt 
-#echo $ts_epoch
-#epoch2timestamp $ts_epoch
-#echo $stamp
+
+# START OF SCRIPT
 
 # getting data
 files=$(find $data_dir -maxdepth 1 \( -name "*.BRT" -o -name "*.BLB" -o -name "*.HKD" -o -name "*.MET" -o -name "*.IRT" \) -mmin -$consider_last_n_min)
@@ -52,13 +50,15 @@ else
 	exit 1
 fi
 
-copying and renaming files to outdir
+# copying and renaming files to outdir
 for file in $files
 do
-	if [ "$timestamp_style" = "cycle_start" ]
+	# calculate timestamp if still needed
+    if [ "$timestamp_style" = "cycle_start" ]
 	then
 		filename2epoch $file
 		ts_shifted=$((ts_epoch-cycle_start_sec))
+        # use property that in bash "/" is integer division to round to multiples of cycle duration
 		time_for_stamp=$((ts_shifted/cycle_duration_sec*cycle_duration_sec+cycle_start_sec))
 	fi
 	epoch2timestamp $time_for_stamp
