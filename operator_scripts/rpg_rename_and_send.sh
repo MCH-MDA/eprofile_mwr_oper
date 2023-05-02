4180f113e2a6a60fa6c95017ff81d4fb7b2bcdb3
rruefenacht <rruefenacht@users.noreply.github.com>
2023-05-02 09:56:13 +0200

    finish renaming of RPG raw files

--- operator_scripts/rpg_rename_and_send.sh
+++ operator_scripts/rpg_rename_and_send.sh
@@ -1,15 +1,29 @@
+# INPUT
 cycle_start_min=1  # minute when first new cycle of the day starts
 cycle_duration_min=10  # duration of one measurement cycle in minutes
 consider_last_n_min=525600 #just for testing #10  # consider files generated in the last minutes normally equal to cycle duration. For longer periods make sure to use timestamp_style=cycle_start 
 echo "testversion with consider_last_n_min=525600 for sending all files generated in last year. Undo to =10 once test is done"
-data_dir=../data/rpg_rename/
-outdir=../data/rpg_rename/out/
+
+data_dir=../data/rpg_rename/  # folder where original files are located (include tailing slash)
+eprof_dir=../data/rpg_rename/out/  # folder where E-PROFILE files are saved to before being sent to FTP  (include tailing slash)
+prefix_orig=test_
+prefix_eprof=MWR_GRE_A_
+len_timestamp_orig=12
+
 timestamp_style=cycle_start  # cycle_start: assumed start of measurement cycle; min_in: smallest input timestamp matching period. CARE: min_in only works if consider_last_n_min makes sure that files from only one obs cycle are consdiered 
 
-# START OF FUNCTION DEFINITIONS
+# you cannot change the following assumption unless modifying the search pattern
+len_ext=4  #length of extension including the dot
+
+# END OF INPUT
+
+
+
+
+
+# FUNCTION DEFINITIONS
 function filename2epoch {
     local filename=$1
-    local len_ext=${2:-4}
     
     local ts_sec=00
     local ts_min=${filename:(-$(($len_ext+2))):2}
@@ -26,11 +40,16 @@ function epoch2timestamp {
 # END OF FUNCTION DEFINITIONS
 
 
+
+
+
 # START OF SCRIPT
+
+# preparing
+len_prefix_orig=${#prefix_orig}
 
 # getting data
 files=$(find $data_dir -maxdepth 1 \( -name "*.BRT" -o -name "*.BLB" -o -name "*.HKD" -o -name "*.MET" -o -name "*.IRT" \) -mmin -$consider_last_n_min)
-echo $files
 
 # preparing for different timestamp options
 if [ "$timestamp_style" = "min_in" ]
@@ -51,7 +70,7 @@ else
 	exit 1
 fi
 
-# copying and renaming files to outdir
+# copying and renaming files to eprof_dir
 for file in $files
 do
 	# calculate timestamp if still needed
@@ -63,10 +82,12 @@ do
 		time_for_stamp=$((ts_shifted/cycle_duration_sec*cycle_duration_sec+cycle_start_sec))
 	fi
 	epoch2timestamp $time_for_stamp
-	echo $stamp
-	# TODO: copy to outdir and use stamp to replace filename at copy destination. For this need to find out length of stamps in initial and final filename
+    bn_file=$(basename $file)
+    ext_file=${file##*.}
+    file_out=$eprof_dir$prefix_eprof${bn_file:$len_prefix_orig:-$(($len_ext+$len_timestamp_orig))}$stamp.$ext_file
+	cp -v $file $file_out
 done
 
-#TODO: replace prefix filename of files in outdir (use emermet script as example)
+
 #TODO: send to FTP (use emermet script as example)
 
