4ab9aa2860ad4e2976bdd218a816c22d8cc7572a
rruefenacht <rruefenacht@users.noreply.github.com>
2023-06-13 15:10:00 +0200

    fix permission issues to add ssh pubkey to sftp user

--- ewc_setup/sftp_add_user_with_pubkey.sh
+++ ewc_setup/sftp_add_user_with_pubkey.sh
@@ -1,6 +1,6 @@
 # This script adds a new user to a previously configured SFTP usergroup.
 #
-# ./sftp_add_user_with_pubkey.sh USERNAME PUBKEY_FILE
+# sftp_add_user_with_pubkey.sh USERNAME PUBKEY_FILE
 
 uname=$1
 pubkey=$2
@@ -13,7 +13,7 @@ sudo useradd -g $SFTP_GROUPNAME -d /upload -s /sbin/nologin $uname  #don't use -
 
 ## generate his home, add public key and adapt permissions
 sudo mkdir -p $SFTP_ROOTDIR/$uname/.ssh
-cat $pubkey >> $SFTP_ROOTDIR/$uname/.ssh/authorized_keys
+cat $pubkey | sudo tee -a $SFTP_ROOTDIR/$uname/.ssh/authorized_keys
 
 sudo mkdir -p $SFTP_ROOTDIR/$uname/upload
 sudo chown -R root:$SFTP_GROUPNAME $SFTP_ROOTDIR/$uname
